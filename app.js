571960396ece149cc69b741aaf495a138484c586
AyjuYaas <105471543+AyjuYaas@users.noreply.github.com>
2023-12-27 00:06:11 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+const colors = ["green", "red", "yellow", "blue"];
+let level = 0;
+let counter = 0;
+let pattern = [];
+let userPattern = [];
+let isReady = false;
+const header = $("h1")[0];
+
+$(document).keydown(function () {
+  if (!isReady) {
+    isReady = true;
+    generateSequence();
+  }
+});
+
+async function generateSequence() {
+  await sleep(200);
+  level++;
+  userPattern = [];
+  pattern = [];
+  counter = 0;
+  header.innerHTML = "Level " + level;
+  let de = 200;
+  for (let i = 0; i < level; i++) {
+    let random = Math.floor(Math.random() * 4);
+    pattern.push(random);
+    choosenColor = colors[random];
+    $(`#${choosenColor}`).fadeIn(150).fadeOut(150).fadeIn(150);
+    await sleep(400);
+  }
+  console.log(pattern);
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+$(".box").click(function (check) {
+  let userClick = check.currentTarget.classList[1];
+  pressed(userClick);
+  let index = colors.indexOf(userClick);
+  userPattern.push(index);
+  counter++;
+  if (counter === level) {
+    checkAnswer();
+  }
+});
+
+function pressed(userColor) {
+  $(`#${userColor}`).addClass("change");
+  setTimeout(function () {
+    $(`#${userColor}`).removeClass("change");
+  }, 150);
+}
+
+function checkAnswer() {
+  if (!arraysEqual(pattern, userPattern)) {
+    gameOver();
+  } else {
+    generateSequence();
+  }
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+function gameOver() {
+  level = 0;
+  header.innerHTML = "Game Over, Press any key to Start Again!";
+  isReady = false;
+}
